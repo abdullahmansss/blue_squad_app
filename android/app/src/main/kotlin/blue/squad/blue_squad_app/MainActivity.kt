b039c6de7748241902b6bb6010a5c9402749d920
abdullahmansss <abdullah.mansour97@gmail.com>
2021-04-26 00:22:28 +0200

    init

---
+++ android/app/src/main/kotlin/blue/squad/blue_squad_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package blue.squad.blue_squad_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:blue_squad_app/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main()
+{
+  runApp(MyApp());
+}
+
+// 1. state less widget
+// 2. state full widget
+
+class MyApp extends StatelessWidget
+{
+  @override
+  Widget build(BuildContext context)
+  {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
